7c34b5032660c40d6abacfbd593281375226c7c5
Gergő Szuhánszky <szuhigergo@gmail.com>
2025-08-24 13:23:39 +0200

    Setup project + Add simple Express Server.

---
+++ server/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors({ origin: "http://localhost:3000" })); // Allow React frontend
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express server!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
