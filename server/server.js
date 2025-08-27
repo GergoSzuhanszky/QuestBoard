5d9f94b01aecd04b22b05899c897299b428f1e07
Gergő Szuhánszky <szuhigergo@gmail.com>
2025-08-27 21:28:17 +0200

    Add DB connection.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 const path = require("path");
+
+const connectDB = require("./config/db");
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, "swagger.yaml"));
@@ -15,6 +18,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello from Express server!");
 });
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
